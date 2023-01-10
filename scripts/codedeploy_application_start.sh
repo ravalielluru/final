0800a7daa499bde1acbc804a2debe9a94f1a953f
ravalielluru <34638035+ravalielluru@users.noreply.github.com>
2023-01-10 11:45:33 -0500

    Update codedeploy_application_start.sh

--- scripts/codedeploy_application_start.sh
+++ scripts/codedeploy_application_start.sh
@@ -2,4 +2,5 @@
 # Stop all servers and start the server
 pm2 stop all
 #pm2 start /home/ubuntu/my-app1/app/index.js
-pm2 start npm -- start
+#pm2 start npm -- start
+pm2 start npm --name "my-app1" -- start
